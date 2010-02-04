f7549d55a568bf58d9a0b85e011c7a1954d6e7a6
Philip Hallstrom <philip@pjkh.com>
2010-02-04 10:35:26 -0800

    add browser check

--- install/app/controllers/admin_controller.rb
+++ install/app/controllers/admin_controller.rb
@@ -1,7 +1,8 @@
 class AdminController < ApplicationController
   layout 'admin'
 
-  before_filter :authenticate, :except => [:login, :logout]
+  before_filter :authenticate, :except => [:login]
+  before_filter :browser_check
 
   def authenticate
     @the_admin_user = AdminUser.find(session[:admin_user_id]) rescue nil
@@ -23,6 +24,10 @@ class AdminController < ApplicationController
   end
   private :check_permission
 
+  def browser_check
+    flash[:warning] = "You are using a very old browser. You are strongly encouraged to upgrade." if request.user_agent =~ /MSIE 6/i
+  end
+  private :browser_check
 
 end # of class AdminController
 
