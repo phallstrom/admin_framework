9a70844e4d76c466135deb1cb1480972f0cfbf8b
Philip Hallstrom <philip@pjkh.com>
2010-02-04 10:39:48 -0800

    updates

--- install/public/javascripts/admin/base.js
+++ install/public/javascripts/admin/base.js
@@ -1,36 +1,22 @@
-/*
- * jQuery implementation
- *
- */
-/*
-  function alternate_table_rows(table) {
-    if ( table == undefined ) {
-      table = "table.data"
-    }
-    $((table + ' tbody tr:nth-child(even)')).removeClass('odd').addClass('even')
-    $((table + ' tbody tr:nth-child(odd)')).removeClass('even').addClass('odd')
+function alternate_table_rows(table) {
+  if ( table == undefined ) {
+    table = "table.data"
   }
-
-  $(document).ready(function() { alternate_table_rows() });
-*/
-
-/* 
- * Prototype implementation
- */
-function alternate_table_rows() {
-  $('table.data tbody tr:nth-child(even)').each( 
-    function(e) { 
-      e.removeClassName('odd')
-      e.addClassName('even');
-    }
-  );
-
-  $('table.data tbody tr:nth-child(odd)').each( 
-    function(e) { 
-      e.removeClassName('even')
-      e.addClassName('odd');
-    }
-  );
+  $((table + ' tbody tr:nth-child(even)')).removeClass('odd').addClass('even')
+  $((table + ' tbody tr:nth-child(odd)')).removeClass('even').addClass('odd')
 }
 
-Event.observe(window, 'load', alternate_table_rows);
+$(document).ready(function() { 
+
+  alternate_table_rows() 
+
+  // Automagically collapse/expand fieldsets in forms to make them easier to manage
+  $('form fieldset.collapsable').
+    find('legend').
+    bind('click', function(e) { $(this).parent().find('div.field, div.fieldset_contents, span.instructions').toggle() }).
+    end().
+    filter('.collapsed').not(':has(div.fieldWithErrors)').find('div.field, div.fieldset_contents, span.instructions').hide().
+    end().
+    prepend("<span class='instructions'>Click the label above to expand this section.</span>")
+
+})
